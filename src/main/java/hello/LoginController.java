0dcf080097c109d41f8dbd07de2eb5d9b33f5883
Madia <bhargav.madia@siemens.com>
2020-08-09 17:13:57 +0530

    spring example with login form

---
+++ src/main/java/hello/LoginController.java
@@ -0,0 +1,22 @@
+package com.demo.duke;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class LoginController {
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@RequestParam String user, @RequestParam String password) {//Welcome page, non-rest]
+		System.out.println("user ---------"+user);
+		System.out.println("password ---------"+password);
+		if(user.equals("parth") && password.equals("parth@123"))
+			return "Welcome "+user+ " , login successful";
+        return "Login failed. Please try again...";
+    }
+	
+	
+}
