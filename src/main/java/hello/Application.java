61f3b9f379613076533069ed57401fbc56613695
Madia <bhargav.madia@siemens.com>
2020-08-09 17:44:36 +0530

    screenshots

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,16 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
-@RestController
+
 public class Application {
-
-  @RequestMapping("/")
-  public String home() {
-    return "Hello Docker World";
-  }
-
+	
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
